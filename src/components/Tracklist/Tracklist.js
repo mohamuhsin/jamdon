6e65dd69a5da63b972a9abf42d20d17bdc1232cb
mohamuhsin <muhsinohoho@outlook.com>
2023-06-01 20:35:05 +0300

    committed

---
+++ src/components/Tracklist/Tracklist.js
@@ -0,0 +1,27 @@
+import React from React;
+
+const tracks = [ {
+    name: name1,
+    artist: artist1,
+    album: album1,
+    id: 1
+},
+{
+name: name2,
+artist: artist2,
+album: album2,
+id: 2
+},
+{
+    name: name3,
+    artist: artist3,
+    album: album3,
+    id: 3
+    }
+    
+]
+const Tracklist = () => {
+    return<></>;
+}
+
+export default Tracklist;
